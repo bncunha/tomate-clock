ccb4a8e199ddb1f9e3f64f0398f5a22b49ff8caf
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-02-16 10:03:00 -0300

    progress timer bar

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, QueryList, Input, Renderer2 } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-timer',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  showLeftSide = false;
+  private maxLeftRotation = 360;
+  private maxRightRotation = 0;
+
+  private leftRotation = 0;
+  private rightRotation = -180;
+
   constructor() { }
 
   ngOnInit() {
+    this.moveTimer();
+  }
+
+  moveTimer() {
+    setInterval(() => {
+      if (this.leftRotation < this.maxLeftRotation) {
+        this.leftRotation++;
+      }
+      if (this.rightRotation < this.maxRightRotation) {
+        this.rightRotation++;
+      } else {
+        this.showLeftSide = true;
+      }
+    }, 10);
+  }
+
+  getRotationString(side: 'LEFT' | 'RIGHT') {
+    return `rotate(${ side === 'LEFT' ? this.leftRotation : this.rightRotation }deg)`;
   }
 
 }
